45b1a4902c534f7595a3564b0242411b4c28be03
linomendesba <juscelinomendesba@gmail.com>
2025-04-01 05:21:30 -0300

    testessss

--- redirecionar.js
+++ redirecionar.js
@@ -17,6 +17,7 @@ function redirecionar(select) {
         url === "https://www.betstat.site/mult365.html" ||
         url === "https://www.betstat.site/365fixo.html" ||
         url === "https://www.betstat.site/multbetano.html" ||
+        url === "https://www.betstat.site/maximabetano.html" ||
         url === "https://www.betstat.site/porcentagembetano.html" ||
         url === "https://www.betstat.site/macrobetano.html" ||
         url === "https://www.betstat.site/betanofixo.html" ||
