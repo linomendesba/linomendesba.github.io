ded617761da6a8bbcfaa0a3fbfb401fec988494d
Lino Mendes <linomendesba@gmail.com>
2025-08-06 17:26:10 -0300

    okokokok

--- redirecionar.js
+++ redirecionar.js
@@ -18,6 +18,7 @@ function redirecionar(select) {
         url === "https://www.betstat.site/buscadortime.html" ||
         url === "https://www.betstat.site/betanomaximasconfronto.html" ||
         url === "https://www.betstat.site/maxima365.html" ||
+                url === "https://www.betstat.site/quadrantesgols.html" ||
         url === "https://www.betstat.site/365macro.html" ||
         url === "https://www.betstat.site/mult365.html" ||
                 url === "https://www.betstat.site/Palpites.html" ||
