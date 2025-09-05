b31c145c3f5408ae821b80522caf9f9a6a72df54
linomendesba <juscelinomendesba@gmail.com>
2025-09-05 11:52:59 -0300

    finalizadokiron

--- redirecionar.js
+++ redirecionar.js
@@ -16,10 +16,12 @@ function redirecionar(select) {
     url === "https://www.betstat.site/equipes.html" ||
     url === "https://www.betstat.site/porcentagem365.html" ||
     url === "https://www.betstat.site/fixokiron.html" ||
+    url === "https://www.betstat.site/buscadoroddkiron.html" ||
     url === "https://www.betstat.site/buscadortime.html" ||
     url === "https://www.betstat.site/betanomaximasconfronto.html" ||
     url === "https://www.betstat.site/maxima365.html" ||
     url === "https://www.betstat.site/porcentagemkiron.html" ||
+    url === "https://www.betstat.site/buscadortimekiron.html" ||
     url === "https://www.betstat.site/quadrantesgols.html" ||
     url === "https://www.betstat.site/tvkiron/" ||
     url === "https://www.betstat.site/365macro.html" ||
