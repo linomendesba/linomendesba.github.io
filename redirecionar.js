3434c71424e20eed0b7797dbc4c7de81db44be3b
Lino Mendes <linomendes@gmail.com>
2025-11-24 05:20:46 -0300

    simula

--- redirecionar.js
+++ redirecionar.js
@@ -9,6 +9,7 @@ function redirecionar(select) {
     url === "gestao.html" ||
     url === "https://www.betstat.site/tv/" ||
     url === "https://www.betstat.site/multkiron.html" ||
+    url === "https://www.betstat.site/estrelasimulador.html" ||
     url === "https://www.betstat.site/estrelastat.html" ||
     url === "https://www.betstat.site/estrelagf.html" ||
     url === "https://www.betstat.site/betsduelo.html" ||
