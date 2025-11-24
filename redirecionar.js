df71bac4df9e87707c46252739e35cd8c54273ee
Juscelino Mendes <juscelinomendesba@gmail.com>
2025-11-24 06:40:13 -0300

    time

--- redirecionar.js
+++ redirecionar.js
@@ -9,6 +9,7 @@ function redirecionar(select) {
     url === "gestao.html" ||
     url === "https://www.betstat.site/tv/" ||
     url === "https://www.betstat.site/multkiron.html" ||
+    url === "https://www.betstat.site/estrelatimeminuto.html" ||
     url === "https://www.betstat.site/estrelasimulador.html" ||
     url === "https://www.betstat.site/estrelaclassifica.html" ||
     url === "https://www.betstat.site/estrelastat.html" ||
