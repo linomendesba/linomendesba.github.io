fbcc84f0d2c7cd645ffcc852e3163e6c62ca9a96
Lino Mendes <linomendes@gmail.com>
2025-12-28 06:18:30 -0300

    jghhgjjgh

--- redirecionar.js
+++ redirecionar.js
@@ -5,9 +5,23 @@ function redirecionar(select) {
   if (
     url === "https://t.me/betstatsite" ||
     url === "https://t.me/ProPlayerFut" ||
+    url === "https://t.me/+DlBr7IVFBk0yZTRh" ||
+    url === "https://t.me/+v8-Zj8xsnec0ZGMx" ||
+    url === "https://t.me/+BxTdgAR89hAxMDAx" ||
+    url === "https://t.me/tip_zera" ||
+    url === "https://t.me/+grZB16eZ9ZJkNjVh" ||
+    url === "https://youtu.be/oRqi3RvH8zE" ||
     url === "blog.html" ||
     url === "gestao.html" ||
+    url === "https://www.bet365.bet.br/#/AVR/B146/R^1/" ||
+    url === "https://br4.bet.br/sports/overview" ||
+    url === "https://www.estrelabet.bet.br/aposta-esportiva?page=virtual" ||
+    url === "https://goldebet.bet.br/sports#/virtual" ||
+    url === "https://www.betano.bet.br/virtuals/futebol/" ||
     url === "https://www.betstat.site/tv/" ||
+    url === "https://www.betstat.site/tvkiron/" ||
+    url === "https://www.betstat.site/central/" ||
+    url === "https://www.betstat.site/geradorbet365/" ||
     url === "https://www.betstat.site/multkiron.html" ||
     url === "https://www.betstat.site/morfeus.html" ||
     url === "https://www.betstat.site/calculadora.html" ||
@@ -15,13 +29,10 @@ function redirecionar(select) {
     url === "https://www.betstat.site/365sequencia.html" ||
     url === "https://www.betstat.site/365raking.html" ||
     url === "https://www.betstat.site/365maximas.html" ||
-    url === "https://www.bet365.bet.br/#/AVR/B146/R^1/" ||
-    url === "https://br4.bet.br/sports/overview" ||
     url === "https://www.betstat.site/365fluxo.html" ||
     url === "https://www.betstat.site/estrelaquadrante.html" ||
     url === "https://www.betstat.site/365localizAI.html" ||
     url === "https://www.betstat.site/mult365.html" ||
-    url === "https://www.betstat.site/estrelaradar.htmll" ||
     url === "https://www.betstat.site/estrelapalpites.html" ||
     url === "https://www.betstat.site/365confronto.html" ||
     url === "https://www.betstat.site/365radar.html" ||
@@ -65,7 +76,6 @@ function redirecionar(select) {
     url === "https://www.betstat.site/kironft.html" ||
     url === "https://www.betstat.site/betsodds.html" ||
     url === "https://www.betstat.site/estrelasequencia.html" ||
-    url === "https://www.betstat.site/estrelaequipes.html" ||
     url === "https://www.betstat.site/betstimes.html" ||
     url === "https://www.betstat.site/kironprojecao.html" ||
     url === "https://www.betstat.site/kironradar.html" ||
@@ -93,39 +103,26 @@ function redirecionar(select) {
     url === "https://www.betstat.site/estrelabuscador.html" ||
     url === "https://www.betstat.site/estrelaht.html" ||
     url === "https://www.betstat.site/kironmacro.html" ||
-    url === "https://t.me/+DlBr7IVFBk0yZTRh" ||
     url === "https://www.betstat.site/maximabets.html" ||
     url === "https://www.betstat.site/golsbets.html" ||
     url === "https://www.betstat.site/rakingbets.html" ||
     url === "https://www.betstat.site/graficoftkiron.html" ||
     url === "https://www.betstat.site/kironbuscador.html" ||
-    url === "https://t.me/+v8-Zj8xsnec0ZGMx" ||
     url === "https://www.betstat.site/estrelafixo.html" ||
     url === "https://www.betstat.site/localizAIbets.html" ||
-    url === "https://www.betstat.site/https://www.betstat.site/kironht.html" ||
-    url === "https://t.me/+BxTdgAR89hAxMDAx" ||
-    url === "https://youtu.be/oRqi3RvH8zE" ||
-    url === "https://www.betstat.site/central/" ||
-    url === "https://t.me/tip_zera" ||
-    url === "https://t.me/+grZB16eZ9ZJkNjVh" ||
     url === "https://www.betstat.site/sequencia.html" ||
     url === "https://www.betstat.site/equipes.html" ||
     url === "https://www.betstat.site/porcentagem365.html" ||
     url === "https://www.betstat.site/fixokiron.html" ||
-    url === "https://www.betstat.site/buscadoroddkiron.html" ||
-    url === "https://www.betstat.site/morfeus.html" ||
     url === "https://www.betstat.site/buscadoroddkiron.html" ||
     url === "https://www.betstat.site/buscadortime.html" ||
     url === "https://www.betstat.site/betanomaximasconfronto.html" ||
     url === "https://www.betstat.site/maxima365.html" ||
     url === "https://www.betstat.site/golsestrela.html" ||
     url === "https://www.betstat.site/porcentagemkiron.html" ||
-    url === "https://www.estrelabet.bet.br/aposta-esportiva?page=virtual" ||
     url === "https://www.betstat.site/buscadortimekiron.html" ||
     url === "https://www.betstat.site/quadrantesgols.html" ||
     url === "https://www.betstat.site/buscadortimestar.html" ||
-    url === "https://www.betstat.site/tvkiron/" ||
-    url === "https://www.betstat.site/365macro.html" ||
     url === "https://www.betstat.site/quadrantestar.html" ||
     url === "https://www.betstat.site/classificacaoestrela.html" ||
     url === "https://www.betstat.site/kironquadrantes.html" ||
@@ -135,9 +132,8 @@ function redirecionar(select) {
     url === "https://www.betstat.site/porcentagemstar.html" ||
     url === "https://www.betstat.site/kironmaximafonfronto.html" ||
     url === "https://www.betstat.site/buscadoroddstar.html" ||
-     url === "https://www.betstat.site/localizAIkiron.html" ||
-     url === "https://www.betstat.site/kirongraficoood.html" ||
-    url === "https://www.betstat.site/mult365.html" ||
+    url === "https://www.betstat.site/localizAIkiron.html" ||
+    url === "https://www.betstat.site/kirongraficoood.html" ||
     url === "https://www.betstat.site/localizAI.html" ||
     url === "https://www.betstat.site/classificacao.html" ||
     url === "https://www.betstat.site/365palpites.html" ||
@@ -148,11 +144,7 @@ function redirecionar(select) {
     url === "https://www.betstat.site/365fixo.html" ||
     url === "https://www.betstat.site/365gols.html" ||
     url === "https://www.betstat.site/graficotime.html" ||
-    url === "https://goldebet.bet.br/sports#/virtual" ||
-    url === "https://www.betano.bet.br/virtuals/futebol/" ||
     url === "https://www.betstat.site/Palpites.html" ||
-    url === "https://www.betsson.bet.br/esportes-virtuais/todos-jogos-esportes-virtuais/virtual-football-nations-cup" ||
-    url === "https://www.betstat.site/365fixo.html" ||
     url === "https://www.betstat.site/graficoodd.html" ||
     url === "https://www.betstat.site/graficosgols.html" ||
     url === "https://www.betstat.site/multbetano.html" ||
@@ -164,26 +156,23 @@ function redirecionar(select) {
     url === "https://www.betstat.site/simulador.html" ||
     url === "https://www.betstat.site/betanofixo.html" ||
     url === "https://www.betstat.site/LigaStat.html" ||
-    url === "https://www.betstat.site/365radar.html" ||
     url === "https://www.betstat.site/365maximaconfronto.html" ||
     url === "https://www.betstat.site/ranking.html" ||
     url === "https://www.betstat.site/365fiboht.html" ||
     url === "https://www.betstat.site/365tips.html" ||
-    url === "https://www.betstat.site/365fiboexato.html" || 
-    url === "https://www.betstat.site/geradorbet365/" || 
-    url === "https://www.betstat.site/classificacaokiron.html" || 
-    url === "https://www.betstat.site/kirontips.html" || 
-    url === "https://www.betstat.site/365tips.html" || 
-    url === "https://www.betstat.site/rankingkiron.html" || 
-    url === "https://www.betstat.site/betanofiboht.html" || 
-    url === "https://www.betstat.site/LigaStatkiron.html" || 
-    url === "https://www.betstat.site/betanobuscador.html" || 
-    url === "https://www.betstat.site/buscadorodd.html" || 
-    url === "https://www.betstat.site/365buscador.html" || 
-    url === "https://www.betstat.site/golskiron.html" || 
-    url === "https://www.betstat.site/gols.html" || 
-    url === "https://www.betstat.site/betanotips.html" || 
-    url === "https://www.betstat.site/betanofiboexato.html" 
+    url === "https://www.betstat.site/365fiboexato.html" ||
+    url === "https://www.betstat.site/classificacaokiron.html" ||
+    url === "https://www.betstat.site/kirontips.html" ||
+    url === "https://www.betstat.site/rankingkiron.html" ||
+    url === "https://www.betstat.site/betanofiboht.html" ||
+    url === "https://www.betstat.site/LigaStatkiron.html" ||
+    url === "https://www.betstat.site/betanobuscador.html" ||
+    url === "https://www.betstat.site/buscadorodd.html" ||
+    url === "https://www.betstat.site/365buscador.html" ||
+    url === "https://www.betstat.site/golskiron.html" ||
+    url === "https://www.betstat.site/gols.html" ||
+    url === "https://www.betstat.site/betanotips.html" ||
+    url === "https://www.betstat.site/betanofiboexato.html"
   ) {
     window.open(url, "_blank");
   } else if (url) {
