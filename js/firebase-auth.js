1027df74450a44e2648fb1087aed4056fe7fb674
Lino Mendes <linomendes@gmail.com>
2026-03-27 19:30:48 -0300

    FINALLL

--- js/firebase-auth.js
+++ js/firebase-auth.js
@@ -1,362 +1,168 @@
-function _0x229c() {
-    const _0x5b98fe = [
-        'location',
-        'gin:',
-        '/home.html',
-        'spositivo:',
-        'alizando\x20l',
-        'lbmBw',
-        'ajCHC',
-        '2661239HzYYiu',
-        'ão\x20alterad',
-        'data',
-        '8829:web:a',
-        'FDyEB',
-        'Erro\x20no\x20lo',
-        'ywkvk',
-        'eQtcu',
-        'RKJWn',
-        'c5c2b1ba66',
-        'tenticado:',
-        'Erro\x20ao\x20re',
-        'bOQtf',
-        'zer\x20login:',
-        'APqaQ',
-        'wYGyW',
-        'autenticaç',
-        'sXvkb',
-        'quqJQ',
-        '0b-zlZepXj',
-        'ZAMEU',
-        'ge.app',
-        'qPzyq',
-        'device_',
-        'Erro\x20ao\x20sa',
-        'e651f9d75d',
-        'qDmWX',
-        'now',
-        '1:94605764',
-        'HfBHn',
-        'Logado',
-        'linoautent',
-        'uWTKj',
-        'OjLpF',
-        'exists',
-        'dcSEx',
-        'motOT',
-        'wikkD',
-        'MBnUa2YOdP',
-        'icador',
-        'ebasestora',
-        'Estado\x20de\x20',
-        'random',
-        'uid',
-        'nwEmr',
-        'ogout.',
-        'user',
-        'RZxmB',
-        'icador.fir',
-        'm-zlV477A',
-        'nmsXT',
-        'auth.html',
-        'getItem',
-        'Sessão\x20ini',
-        'uZjyb',
-        'logout',
-        '117BEPmlU',
-        'toISOStrin',
-        'users',
-        'ICnhD',
-        'Erro\x20ao\x20fa',
-        'UBPMq',
-        'setItem',
-        'utro\x20dispo',
-        '7664Rjahkg',
-        'gXOjP',
-        'zoqeV',
-        'ThLMx',
-        'vYRGx',
-        'tYOpG',
-        'login',
-        'ZMsvB',
-        '723252YkBfVG',
-        'AIzaSyDmLG',
-        'QIAci',
-        'zVyil',
-        'OtPKp',
-        '10VegZKe',
-        'wTdTS',
-        'Usuário\x20au',
-        '9460576488',
-        'YCpms',
-        '255321DbSsXe',
-        'EYmBq',
-        'pvCRL',
-        'Deslogado',
-        'ebaseapp.c',
-        '285708NfKDCM',
-        'deviceId',
-        'OdCUr',
-        'fRROC',
-        'ir:',
-        'TMnhO',
-        'catch',
-        'message',
-        '/auth.html',
-        'gistrar\x20di',
-        'toString',
-        'substring',
-        'removeItem',
-        'IxKaN',
-        'href',
-        'zjTKK',
-        'log',
-        'ciada\x20em\x20o',
-        '1123475ABtfsE',
-        'fXAeD',
-        'sitivo.\x20Re',
-        'G-4WT805FF',
-        'MPuTD',
-        '37386JAiZPw',
-        'then',
-        'dPrkj',
-        'pathname',
-        'error'
-    ];
-    _0x229c = function () {
-        return _0x5b98fe;
-    };
-    return _0x229c();
-}
-const _0xa87fda = _0x3a14;
-(function (_0x45e590, _0xb7492) {
-    const _0x340265 = _0x3a14, _0x85ff31 = _0x45e590();
-    while (!![]) {
-        try {
-            const _0x926a1 = parseInt(_0x340265(0x173)) / (0x164 * 0xd + -0x137a + -0x167 * -0x1) + -parseInt(_0x340265(0x16e)) / (-0x3 * -0x1e7 + 0x222 + -0x7d5) * (parseInt(_0x340265(0x18f)) / (-0x1 * -0x68e + 0x6a1 + -0x119 * 0xc)) + -parseInt(_0x340265(0x169)) / (-0x3 * 0xc3b + 0xc81 + 0x1834) + -parseInt(_0x340265(0x18a)) / (0x6 * 0x49 + -0x1487 + 0x12d6) + parseInt(_0x340265(0x178)) / (-0x29 * -0x2a + 0x1f3f + 0x25f3 * -0x1) + parseInt(_0x340265(0x19b)) / (0xc9d + 0x89e * 0x2 + -0x1dd2 * 0x1) + -parseInt(_0x340265(0x161)) / (0x2 * 0xf9a + 0x1415 + -0x3341) * (-parseInt(_0x340265(0x159)) / (0x12 * 0x4f + 0x8e4 + -0xd9 * 0x11));
-            if (_0x926a1 === _0xb7492)
-                break;
-            else
-                _0x85ff31['push'](_0x85ff31['shift']());
-        } catch (_0x3a35d3) {
-            _0x85ff31['push'](_0x85ff31['shift']());
-        }
-    }
-}(_0x229c, 0x60529 + 0x140c1 + -0x3cc42));
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
 import {
-    getAuth,
-    onAuthStateChanged,
-    signOut,
-    signInWithEmailAndPassword
-} from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js';
+  getAuth,
+  onAuthStateChanged,
+  signOut,
+  signInWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js";
 import {
-    getFirestore,
-    doc,
-    setDoc,
-    onSnapshot
-} from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js';
+  getFirestore,
+  doc,
+  setDoc,
+  getDoc,
+  onSnapshot
+} from "https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js";
+
+// Configuração do Firebase
 const firebaseConfig = {
-        'apiKey': _0xa87fda(0x16a) + _0xa87fda(0x147) + _0xa87fda(0x134) + _0xa87fda(0x152),
-        'authDomain': _0xa87fda(0x140) + _0xa87fda(0x151) + _0xa87fda(0x177) + 'om',
-        'projectId': _0xa87fda(0x140) + _0xa87fda(0x148),
-        'storageBucket': _0xa87fda(0x140) + _0xa87fda(0x151) + _0xa87fda(0x149) + _0xa87fda(0x136),
-        'messagingSenderId': _0xa87fda(0x171) + '29',
-        'appId': _0xa87fda(0x13d) + _0xa87fda(0x19e) + _0xa87fda(0x12a) + _0xa87fda(0x13a) + '4',
-        'measurementId': _0xa87fda(0x18d) + 'HQ'
-    }, app = initializeApp(firebaseConfig), auth = getAuth(app), db = getFirestore(app);
-function _0x3a14(_0x153752, _0x13345f) {
-    _0x153752 = _0x153752 - (0x17ec + -0xa2f * 0x3 + -0x298 * -0x3);
-    const _0x576633 = _0x229c();
-    let _0x40ca23 = _0x576633[_0x153752];
-    return _0x40ca23;
-}
-let isRedirecting = ![];
+  apiKey: "AIzaSyDmLGMBnUa2YOdP0b-zlZepXjm-zlV477A",
+  authDomain: "linoautenticador.firebaseapp.com",
+  projectId: "linoautenticador",
+  storageBucket: "linoautenticador.firebasestorage.app",
+  messagingSenderId: "946057648829",
+  appId: "1:946057648829:web:ac5c2b1ba66e651f9d75d4",
+  measurementId: "G-4WT805FFHQ",
+};
+
+// Inicializa o Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+// Variável para controlar o estado de redirecionamento
+let isRedirecting = false;
+
+// Função para gerar ou recuperar um ID único para o dispositivo
 function getDeviceId() {
-    const _0x3f5fa1 = _0xa87fda, _0x552810 = {
-            'RKJWn': _0x3f5fa1(0x179),
-            'motOT': function (_0x579d3b, _0x34a5df) {
-                return _0x579d3b + _0x34a5df;
-            },
-            'dPrkj': function (_0x28916a, _0x39db41) {
-                return _0x28916a + _0x39db41;
-            },
-            'ICnhD': function (_0x4b9e30, _0xada6a3) {
-                return _0x4b9e30 + _0xada6a3;
-            },
-            'OdCUr': _0x3f5fa1(0x138)
-        };
-    let _0x56612a = localStorage[_0x3f5fa1(0x155)](_0x552810[_0x3f5fa1(0x129)]);
-    return !_0x56612a && (_0x56612a = _0x552810[_0x3f5fa1(0x145)](_0x552810[_0x3f5fa1(0x191)](_0x552810[_0x3f5fa1(0x15c)](_0x552810[_0x3f5fa1(0x17a)], Date[_0x3f5fa1(0x13c)]()), '_'), Math[_0x3f5fa1(0x14b)]()[_0x3f5fa1(0x182)](-0x74c + -0x1bf9 + 0x25 * 0xf5)[_0x3f5fa1(0x183)](-0x1e64 + 0x2443 + -0x5dd * 0x1, -0x81 + -0x20ca + 0xb1e * 0x3)), localStorage[_0x3f5fa1(0x15f)](_0x552810[_0x3f5fa1(0x129)], _0x56612a)), _0x56612a;
+  let deviceId = localStorage.getItem("deviceId");
+  if (!deviceId) {
+    deviceId = "device_" + Date.now() + "_" + Math.random().toString(36).substring(2, 15);
+    localStorage.setItem("deviceId", deviceId);
+  }
+  return deviceId;
 }
-async function registerCurrentDevice(_0x1bef61) {
-    const _0x1758bb = _0xa87fda, _0x1d8474 = {
-            'wikkD': function (_0x3924eb, _0x2bb510, _0x500e14, _0x56d14c) {
-                return _0x3924eb(_0x2bb510, _0x500e14, _0x56d14c);
-            },
-            'APqaQ': _0x1758bb(0x15b),
-            'tYOpG': function (_0x31c19f) {
-                return _0x31c19f();
-            },
-            'YCpms': function (_0x233710, _0x3e8e93, _0x3c9ba6, _0x3550d9) {
-                return _0x233710(_0x3e8e93, _0x3c9ba6, _0x3550d9);
-            },
-            'zoqeV': _0x1758bb(0x12c) + _0x1758bb(0x181) + _0x1758bb(0x197)
-        };
-    if (!_0x1bef61)
-        return ![];
-    const _0x417d1b = _0x1d8474[_0x1758bb(0x146)](doc, db, _0x1d8474[_0x1758bb(0x12f)], _0x1bef61[_0x1758bb(0x14c)]), _0x3c0f3c = _0x1d8474[_0x1758bb(0x166)](getDeviceId);
-    try {
-        return await _0x1d8474[_0x1758bb(0x172)](setDoc, _0x417d1b, {
-            'deviceId': _0x3c0f3c,
-            'lastLogin': new Date()[_0x1758bb(0x15a) + 'g'](),
-            'lastLoginTime': Date[_0x1758bb(0x13c)]()
-        }, { 'merge': !![] }), !![];
-    } catch (_0x4517fe) {
-        return console[_0x1758bb(0x193)](_0x1d8474[_0x1758bb(0x163)], _0x4517fe), ![];
+
+// Função para registrar este dispositivo como o atual
+async function registerCurrentDevice(user) {
+  if (!user) return false;
+  
+  const userRef = doc(db, "users", user.uid);
+  const deviceId = getDeviceId();
+  
+  try {
+    // Sempre atualizamos com o dispositivo atual
+    await setDoc(
+      userRef,
+      { 
+        deviceId: deviceId, 
+        lastLogin: new Date().toISOString(),
+        lastLoginTime: Date.now() // Timestamp numérico para comparações
+      },
+      { merge: true }
+    );
+    
+    return true;
+  } catch (error) {
+    console.error("Erro ao registrar dispositivo:", error);
+    return false;
+  }
+}
+
+// Configurar listener para monitorar alterações no documento do usuário
+function setupDeviceMonitor(user) {
+  if (!user) return;
+  
+  const userRef = doc(db, "users", user.uid);
+  const deviceId = getDeviceId();
+  
+  // Retorna a função de cancelamento para poder desativar o listener mais tarde
+  return onSnapshot(userRef, (doc) => {
+    if (isRedirecting) return; // Evita múltiplos redirecionamentos
+    
+    if (doc.exists()) {
+      const data = doc.data();
+      
+      // Se o deviceId no documento for diferente do atual, este dispositivo deve fazer logout
+      if (data.deviceId && data.deviceId !== deviceId) {
+        console.log("Detectada sessão em outro dispositivo. Realizando logout.");
+        isRedirecting = true;
+        
+        // Limpa o ID do dispositivo local e desconecta
+        localStorage.removeItem("deviceId");
+        signOut(auth).then(() => {
+          // Usar setTimeout para evitar problemas com redirecionamentos sobrepostos
+          setTimeout(() => {
+            window.location.href = "auth.html";
+          }, 100);
+        });
+      }
     }
+  });
 }
-function setupDeviceMonitor(_0x5b3877) {
-    const _0x5728fe = _0xa87fda, _0x347931 = {
-            'dcSEx': _0x5728fe(0x154),
-            'ZMsvB': function (_0x5b95b5, _0x5c82d3, _0x1b3fb4) {
-                return _0x5b95b5(_0x5c82d3, _0x1b3fb4);
-            },
-            'IxKaN': function (_0x8e49cc, _0x7d6c82) {
-                return _0x8e49cc !== _0x7d6c82;
-            },
-            'TMnhO': _0x5728fe(0x156) + _0x5728fe(0x189) + _0x5728fe(0x160) + _0x5728fe(0x18c) + _0x5728fe(0x198) + _0x5728fe(0x14e),
-            'fXAeD': _0x5728fe(0x179),
-            'sXvkb': function (_0x46ca36, _0x370e43) {
-                return _0x46ca36(_0x370e43);
-            },
-            'zjTKK': function (_0x9b3fd, _0x25fc48, _0x5da653, _0x43a740) {
-                return _0x9b3fd(_0x25fc48, _0x5da653, _0x43a740);
-            },
-            'ZAMEU': _0x5728fe(0x15b),
-            'uWTKj': function (_0x1bb9e0) {
-                return _0x1bb9e0();
-            },
-            'QIAci': function (_0x4b2220, _0x1ced82, _0x42f751) {
-                return _0x4b2220(_0x1ced82, _0x42f751);
-            }
-        };
-    if (!_0x5b3877)
-        return;
-    const _0xb440df = _0x347931[_0x5728fe(0x187)](doc, db, _0x347931[_0x5728fe(0x135)], _0x5b3877[_0x5728fe(0x14c)]), _0x3bd7c7 = _0x347931[_0x5728fe(0x141)](getDeviceId);
-    let _0x5c517a = !![];
-    return _0x347931[_0x5728fe(0x16b)](onSnapshot, _0xb440df, _0xacb35d => {
-        const _0x5c0b12 = _0x5728fe, _0x18755f = {
-                'bOQtf': _0x347931[_0x5c0b12(0x144)],
-                'wTdTS': function (_0x34d9f2, _0x550605, _0x5021d3) {
-                    const _0x13bf6a = _0x5c0b12;
-                    return _0x347931[_0x13bf6a(0x168)](_0x34d9f2, _0x550605, _0x5021d3);
-                }
-            };
-        if (_0x5c517a) {
-            _0x5c517a = ![];
-            return;
-        }
-        if (isRedirecting)
-            return;
-        if (_0xacb35d[_0x5c0b12(0x143)]()) {
-            const _0x2216da = _0xacb35d[_0x5c0b12(0x19d)]();
-            _0x2216da[_0x5c0b12(0x179)] && _0x347931[_0x5c0b12(0x185)](_0x2216da[_0x5c0b12(0x179)], _0x3bd7c7) && (console[_0x5c0b12(0x188)](_0x347931[_0x5c0b12(0x17d)]), isRedirecting = !![], localStorage[_0x5c0b12(0x184)](_0x347931[_0x5c0b12(0x18b)]), _0x347931[_0x5c0b12(0x132)](signOut, auth)[_0x5c0b12(0x190)](() => {
-                const _0x270f16 = _0x5c0b12;
-                _0x18755f[_0x270f16(0x16f)](setTimeout, () => {
-                    const _0x17cf4f = _0x270f16;
-                    window[_0x17cf4f(0x194)][_0x17cf4f(0x186)] = _0x18755f[_0x17cf4f(0x12d)];
-                }, 0x2 * -0x25f + 0x67 * -0x61 + 0x2c29);
-            }));
-        }
-    });
-}
+
+// Monitora o estado de autenticação
 let unsubscribeDeviceMonitor = null;
-onAuthStateChanged(auth, async _0x42fec8 => {
-    const _0xab69a3 = _0xa87fda, _0xc1c1d3 = {
-            'wYGyW': _0xab69a3(0x14a) + _0xab69a3(0x131) + _0xab69a3(0x19c) + 'o:',
-            'ThLMx': _0xab69a3(0x13f),
-            'gXOjP': _0xab69a3(0x176),
-            'OtPKp': function (_0x54e174) {
-                return _0x54e174();
-            },
-            'nmsXT': function (_0x4f77ce, _0x454f75) {
-                return _0x4f77ce !== _0x454f75;
-            },
-            'ywkvk': _0xab69a3(0x180),
-            'zVyil': _0xab69a3(0x154),
-            'pvCRL': _0xab69a3(0x170) + _0xab69a3(0x12b),
-            'lbmBw': function (_0x32359d, _0x1337a6) {
-                return _0x32359d(_0x1337a6);
-            },
-            'EYmBq': function (_0x2e72d6, _0x109c8a) {
-                return _0x2e72d6 === _0x109c8a;
-            },
-            'nwEmr': function (_0x2a3cb3, _0x31daf6) {
-                return _0x2a3cb3 === _0x31daf6;
-            },
-            'UBPMq': function (_0x2448a8, _0x3aa5a6) {
-                return _0x2448a8 === _0x3aa5a6;
-            },
-            'qDmWX': _0xab69a3(0x196)
-        };
-    console[_0xab69a3(0x188)](_0xc1c1d3[_0xab69a3(0x130)], _0x42fec8 ? _0xc1c1d3[_0xab69a3(0x164)] : _0xc1c1d3[_0xab69a3(0x162)]);
-    unsubscribeDeviceMonitor && (_0xc1c1d3[_0xab69a3(0x16d)](unsubscribeDeviceMonitor), unsubscribeDeviceMonitor = null);
-    if (!_0x42fec8) {
-        !isRedirecting && _0xc1c1d3[_0xab69a3(0x153)](window[_0xab69a3(0x194)][_0xab69a3(0x192)], _0xc1c1d3[_0xab69a3(0x127)]) && (window[_0xab69a3(0x194)][_0xab69a3(0x186)] = _0xc1c1d3[_0xab69a3(0x16c)]);
-        return;
+
+onAuthStateChanged(auth, async (user) => {
+  console.log("Estado de autenticação alterado:", user ? "Logado" : "Deslogado");
+  
+  // Cancelar monitor anterior, se existir
+  if (unsubscribeDeviceMonitor) {
+    unsubscribeDeviceMonitor();
+    unsubscribeDeviceMonitor = null;
+  }
+  
+  if (!user) {
+    // Se não houver usuário e não estamos em processo de redirecionamento
+    if (!isRedirecting && !window.location.href.includes("auth.html")) {
+      window.location.href = "auth.html";
     }
-    console[_0xab69a3(0x188)](_0xc1c1d3[_0xab69a3(0x175)], _0x42fec8[_0xab69a3(0x14c)]), await _0xc1c1d3[_0xab69a3(0x199)](registerCurrentDevice, _0x42fec8), unsubscribeDeviceMonitor = _0xc1c1d3[_0xab69a3(0x199)](setupDeviceMonitor, _0x42fec8);
-    const _0x264835 = window[_0xab69a3(0x194)][_0xab69a3(0x192)];
-    (_0xc1c1d3[_0xab69a3(0x174)](_0x264835, _0xc1c1d3[_0xab69a3(0x127)]) || _0xc1c1d3[_0xab69a3(0x14d)](_0x264835, '/') || _0xc1c1d3[_0xab69a3(0x15e)](_0x264835, '')) && (window[_0xab69a3(0x194)][_0xab69a3(0x186)] = _0xc1c1d3[_0xab69a3(0x13b)]);
-}), window[_0xa87fda(0x167)] = async function (_0x5806c7, _0x9fd499) {
-    const _0x1d88c5 = _0xa87fda, _0x17c564 = {
-            'MPuTD': function (_0x59bb94, _0x58861e, _0x562355, _0x465d39) {
-                return _0x59bb94(_0x58861e, _0x562355, _0x465d39);
-            },
-            'fRROC': _0x1d88c5(0x1a0) + _0x1d88c5(0x195),
-            'OjLpF': function (_0xb6d411, _0x521bb0) {
-                return _0xb6d411(_0x521bb0);
-            },
-            'vYRGx': function (_0x24f4ff, _0x1ce107) {
-                return _0x24f4ff + _0x1ce107;
-            },
-            'qPzyq': _0x1d88c5(0x15d) + _0x1d88c5(0x12e) + '\x20'
-        };
-    try {
-        isRedirecting = ![];
-        const _0x2ff541 = await _0x17c564[_0x1d88c5(0x18e)](signInWithEmailAndPassword, auth, _0x5806c7, _0x9fd499);
-        return _0x2ff541[_0x1d88c5(0x14f)];
-    } catch (_0x799ed8) {
-        console[_0x1d88c5(0x193)](_0x17c564[_0x1d88c5(0x17b)], _0x799ed8), _0x17c564[_0x1d88c5(0x142)](alert, _0x17c564[_0x1d88c5(0x165)](_0x17c564[_0x1d88c5(0x137)], _0x799ed8[_0x1d88c5(0x17f)]));
-        throw _0x799ed8;
-    }
-}, window[_0xa87fda(0x158)] = function () {
-    const _0x1d1863 = _0xa87fda, _0x5586aa = {
-            'quqJQ': _0x1d1863(0x154),
-            'ajCHC': function (_0x126ea3, _0xd3ce80, _0x115268) {
-                return _0x126ea3(_0xd3ce80, _0x115268);
-            },
-            'uZjyb': _0x1d1863(0x139) + _0x1d1863(0x17c),
-            'eQtcu': function (_0x224de4, _0x1e8510, _0x4654f9) {
-                return _0x224de4(_0x1e8510, _0x4654f9);
-            },
-            'RZxmB': _0x1d1863(0x179),
-            'HfBHn': function (_0x3f6dde, _0x2a978e) {
-                return _0x3f6dde(_0x2a978e);
-            }
-        };
-    isRedirecting = !![], localStorage[_0x1d1863(0x184)](_0x5586aa[_0x1d1863(0x150)]), _0x5586aa[_0x1d1863(0x13e)](signOut, auth)[_0x1d1863(0x190)](() => {
-        const _0x25a77c = _0x1d1863, _0x38dc72 = { 'FDyEB': _0x5586aa[_0x25a77c(0x133)] };
-        _0x5586aa[_0x25a77c(0x19a)](setTimeout, () => {
-            const _0x78041d = _0x25a77c;
-            window[_0x78041d(0x194)][_0x78041d(0x186)] = _0x38dc72[_0x78041d(0x19f)];
-        }, 0x8b6 + 0x6ea + 0x514 * -0x3);
-    })[_0x1d1863(0x17e)](_0x2dc253 => {
-        const _0x46ab34 = _0x1d1863;
-        console[_0x46ab34(0x193)](_0x5586aa[_0x46ab34(0x157)], _0x2dc253), _0x5586aa[_0x46ab34(0x128)](setTimeout, () => {
-            const _0x194844 = _0x46ab34;
-            window[_0x194844(0x194)][_0x194844(0x186)] = _0x5586aa[_0x194844(0x133)];
-        }, -0x6b2 * 0x4 + -0x40 * 0x3b + 0x1 * 0x29ec);
+    return;
+  }
+  
+  // Usuário está logado
+  console.log("Usuário autenticado:", user.uid);
+  
+  // Registra este dispositivo
+  await registerCurrentDevice(user);
+  
+  // Configura monitoramento para detectar sessões em outros dispositivos
+  unsubscribeDeviceMonitor = setupDeviceMonitor(user);
+  
+  // Se estamos na página de auth mas o usuário está logado, redireciona para a página principal
+  if (window.location.href.includes("auth.html")) {
+    window.location.href = "/home.html"; // Ajuste para sua página principal
+  }
+});
+
+// Função de login para a página de autenticação
+window.login = async function(email, password) {
+  try {
+    isRedirecting = false; // Reseta o estado de redirecionamento
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    return userCredential.user;
+  } catch (error) {
+    console.error("Erro no login:", error);
+    alert("Erro ao fazer login: " + error.message);
+    throw error;
+  }
+};
+
+// Função de logout
+window.logout = function() {
+  isRedirecting = true;
+  localStorage.removeItem("deviceId");
+  
+  signOut(auth)
+    .then(() => {
+      setTimeout(() => {
+        window.location.href = "auth.html";
+      }, 100);
+    })
+    .catch((error) => {
+      console.error("Erro ao sair:", error);
+      setTimeout(() => {
+        window.location.href = "auth.html";
+      }, 100);
     });
 };
